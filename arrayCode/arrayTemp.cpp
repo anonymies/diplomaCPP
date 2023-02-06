0ff865c69b2e28d5e0895389efb7da8ffac8791b
anonymies <124495695+anonymies@users.noreply.github.com>
2023-02-06 09:13:01 +0800

    Create arrayTemp.cpp

---
+++ arrayCode/arrayTemp.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string name[20];
+	string no_tel[20];
+	int age[20];
+	
+	
+	for(int i = 0; i<20; i++)
+	{
+		cout << "Please enter name: ";
+		cin >> name[i];
+		cout << "Please enter no tel: ";
+		cin >> no_tel[i];
+		cout << "Please enter age: ";
+		cin >> age[i];
+	}
+	
+}
