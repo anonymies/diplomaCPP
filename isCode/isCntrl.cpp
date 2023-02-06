f86775cfb0d55b3399a7fb2d2a4803a103aa9d8a
anonymies <124495695+anonymies@users.noreply.github.com>
2023-02-06 09:29:28 +0800

    Create isCntrl.cpp

---
+++ isCode/isCntrl.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+	int i =0;
+	char str[] = "first line \n second line \n";
+	while (!iscntrl(str[i]))
+	{
+		cout << str[i];
+		i++;
+	}
+	return 0;
+}
